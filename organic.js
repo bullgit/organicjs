5a84054c0166eb04c37a29cedaabe74169a37547
kevingimbel <hallo@kevingimbel.de>
2013-08-20 17:16:49 +0200

    First commit

---
+++ organic.js
@@ -0,0 +1,7 @@
+document.createElement('neck');
+document.createElement('chest');
+document.createElement('heart');
+document.createElement('lung');
+document.createElement('trachea');
+document.createElement('alveoli');
+document.createElement('stomach');
